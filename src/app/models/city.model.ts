71972fed5c79d6f77b6e8b3e759ff4d5810ac173
vikrant <vikrant@ssh.io>
2019-05-02 02:21:50 +0530

    made a city class

--- src/app/models/city.model.ts
+++ src/app/models/city.model.ts
@@ -1,41 +1,39 @@
 
-public class City {
-    int x;
-    int y;
-    
-    // Constructs a randomly placed city
-    public City(){
-        this.x = (int)(Math.random()*200);
-        this.y = (int)(Math.random()*200);
-    }
-    
-    // Constructs a city at chosen x, y location
-    public City(int x, int y){
-        this.x = x;
-        this.y = y;
+export class City {
+    x : number;
+    y : number;
+        
+    constructor( x: number, y: number){
+        if (x == undefined){
+            this.x = Math.random()*200;
+        }
+        else{
+            this.x = x;
+        }
+        if (y == undefined){
+            this.y = Math.random()*200; 
+        }
+        else{
+            this.y = y;
+        }
     }
     
     // Gets city's x coordinate
-    public int getX(){
+    public get X(): number{
         return this.x;
     }
     
     // Gets city's y coordinate
-    public int getY(){
+    public get Y(): number{
         return this.y;
     }
     
     // Gets the distance to given city
-    public double distanceTo(City city){
-        int xDistance = Math.abs(getX() - city.getX());
-        int yDistance = Math.abs(getY() - city.getY());
-        double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+    public distanceTo(city: City): number{
+        let xDistance = Math.abs(this.X - city.X);
+        let  yDistance = Math.abs(this.Y - city.Y);
+        let distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
         
         return distance;
-    }
-    
-    @Override
-    public String toString(){
-        return getX()+", "+getY();
     }
 }
